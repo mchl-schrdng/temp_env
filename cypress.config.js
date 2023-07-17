cf73233549bcebf60948b21fc42f1f0ea4a8926a
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-17 18:47:25 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
-// cypress.config.js
-module.exports = {
-  testingType: "e2e",
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  testingType: 'e2e',
+  baseUrl: process.env.CYPRESS_BASE_URL,
   integrationFolder: "cypress/integration",
   testFiles: "**/*_spec.js"
-}
+})

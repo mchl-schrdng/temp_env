570bc2e3d43695d879b7a74d3cb4306f89704384
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-17 10:40:48 +0200

    updates

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
 def main():
-    st.title('Hello World again')
+    st.title('Hello App')
+
+    user_name = st.text_input('Enter your name')
+    if user_name:
+        st.write(f'Hello, {user_name}!')
 
 if __name__ == '__main__':
     main()

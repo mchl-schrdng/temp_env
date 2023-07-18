bfe4fa5548300a80976ea29d2f08547525548b96
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-18 08:36:16 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: process.env.CYPRESS_BASE_URL,
     specPattern: "cypress/integration/**/*_spec.js",
     supportFile: false
   },

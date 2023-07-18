bd9c8edf495a487592a230f0646953374370773c
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-18 15:38:20 +0200

    Update spec.js

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,7 +2,7 @@
 
 describe('My First Test', () => {
   it('Visits the app', () => {
-    cy.visit('https://streamlit-app-pr-66-iyxvyhs26a-uc.a.run.app');
+    cy.visit(Cypress.env('CYPRESS_BASE_URL'));
 
     cy.contains('My first app'); // assert that the page contains this title
   })

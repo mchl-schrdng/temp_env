c7683262805435784e75ea71ede43130a2224114
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-18 15:37:33 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://streamlit-app-pr-66-iyxvyhs26a-uc.a.run.app',
     specPattern: "cypress/integration/**/*spec.js",
     supportFile: false
   },

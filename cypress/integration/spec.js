593e0edd57f0b3fffeae12d5d7ebfb029ec49718
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-18 15:49:13 +0200

    Update spec.js

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,7 +2,10 @@
 
 describe('My First Test', () => {
   it('Visits the app', () => {
-    cy.visit(Cypress.env('CYPRESS_BASE_URL'));
+    const baseUrl = Cypress.env('CYPRESS_BASE_URL'); // Extract the base URL from Cypress.env('CYPRESS_BASE_URL')
+    const trimmedUrl = baseUrl.split('a.run.app')[0] + 'a.run.app'; // Remove everything after 'a.run.app'
+
+    cy.visit(trimmedUrl);
 
     cy.contains('My first app'); // assert that the page contains this title
   })

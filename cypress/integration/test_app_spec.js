cf8aeacc2470e25ebe5ffcdea43d9a373b03d6c5
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-18 08:36:44 +0200

    Update test_app_spec.js

--- cypress/integration/test_app_spec.js
+++ cypress/integration/test_app_spec.js
@@ -2,7 +2,7 @@
 
 describe('My First Test', () => {
   it('Visits the app', () => {
-    cy.visit(Cypress.env('/'));
+    cy.visit('/');
 
     cy.contains('My first app'); // assert that the page contains this title
 
